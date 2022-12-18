f60d05c84060e934f05cd41270b795a9c9c10d87
Wamaitha <wamaithaweru19@gmail.com>
2022-12-18 14:38:26 +0300

    Creates Component for the Navbar and logo section

---
+++ src/Components/Header/header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import css from "./header.module.css"
+import {TiShoppingCart} from "react-icons/ti"
+import logo from "../../Assets/logo2.png"
+
+function Header() {
+    return (
+      <div className={css.Header}>
+         
+         <div className={css.logo}>
+             <img src={logo} alt="logo" height={100}></img>
+             <span>SkinbLa</span>
+         </div>
+
+         <div className={css.right}>
+             <div className={css.menu}>
+                <ul className={css.menu}>
+                    <li>Brands</li>
+                    <li>Collections</li>
+                    <li>Sales</li>
+                    <li>New</li>
+                    <li>Eng</li>
+                </ul>
+             </div>
+
+            <input type="text" className={css.search} placeholder="Search"></input>
+                    <TiShoppingCart className={css.cart}/>  
+        </div>
+    </div>
+
+      
+    );
+  }
+  
+  export default Header;
+  
