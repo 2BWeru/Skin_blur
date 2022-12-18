df48eb2fc223eca81dad965326c9cd7d421202b4
Wamaitha <wamaithaweru19@gmail.com>
2022-12-18 14:39:14 +0300

    initial commit

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+import img2 from "../Assets/logo.png"
+
+export const SliderProducts = [{
+    name: "SKIN",
+    detail:"Super Skin Care",
+    price:"25",
+    img:img2,
+},
+{
+    name: "SKIN",
+    detail:"Super Skin Care",
+    price:"30",
+    img:img2,
+},
+{
+    name: "SKIN",
+    detail:"Super Skin Care",
+    price:"45",
+    img:img2,
+}
+]
